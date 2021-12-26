762ac02196a85ac1f32ad11b94aba78f361006a1
hyojaejung <hyojaejung@kakao.com>
2021-12-26 22:43:28 +0900

    It is very hard to solve 124country problem...

---
+++ 124_country_numbers.py
@@ -0,0 +1,61 @@
+import math
+
+# def solution(dicimal_number:int):
+#     temp = ''
+#     from_dicimal_system_to_country124_system = {
+#         1:'1',
+#         2:'2',
+#         0:'4',
+#                  }
+#     digit_number = 1
+#     sum_position_numbers = 0
+#     while True:
+#         sum_position_numbers += 3**(digit_number-1)
+#         digit_number += 1
+#         if math.ceil(dicimal_number/3) < sum_position_numbers + 3**(digit_number-1):
+#             break
+
+#     print(digit_number)
+
+#     temp += str(from_dicimal_system_to_country124_system[dicimal_number%3])
+#     dicimal_number = math.ceil(dicimal_number/3) - sum_position_numbers
+    
+#     while True:
+#         if dicimal_number >= 3:
+#             temp += from_dicimal_system_to_country124_system[dicimal_number%3]
+#             dicimal_number = math.ceil(dicimal_number/3)
+#         if dicimal_number < 3:
+#             temp += from_dicimal_system_to_country124_system[dicimal_number]
+#             break
+
+#     while len(temp) < digit_number:
+#         temp += '1'
+#     answer = ''
+#     for i in reversed(list(temp)):
+#         answer += i
+
+#     return answer
+
+def solution(dicimal_number:int):
+    digit_number = 1
+    sum_digit_numbers = 1
+    while dicimal_number//sum_digit_numbers > 0:
+        sum_digit_numbers += 3**digit_number
+        digit_number += 1
+    print(digit_number,sum_digit_numbers)
+    
+
+
+solution(1)
+solution(2)
+solution(3)
+solution(4)
+solution(5)
+solution(6)
+solution(7)
+solution(8)
+solution(9)
+solution(10)
+solution(11)
+solution(12)
+solution(13)
