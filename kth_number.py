c713b2d4ee8c7fb5609ddcc795f3110c1ef4f9f7
hyojaejung <hyojaejung@kakao.com>
2021-12-28 21:02:24 +0900

    add level1

---
+++ kth_number.py
@@ -0,0 +1,14 @@
+# def solution(array, commands):
+#     answer = []
+#     for i in commands:
+#         temp = array[i[0]-1:i[1]]
+#         temp.sort()
+#         answer.append(temp[i[2]-1])
+#     return answer
+
+def solution(array, commands):
+    return [sorted(array[i[0]-1:i[1]])[i[2]-1] for i in commands]
+
+
+
+print(solution([1, 5, 2, 6, 3, 7, 4],[[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
