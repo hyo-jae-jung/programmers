367b144f95c5d3509852fe961a05a1b4279238d2
hyojaejung <hyojaejung@kakao.com>
2022-10-04 07:09:07 +0900

    learn sort!

---
+++ 문자열_내_마음대로_정렬하기.py
@@ -0,0 +1,14 @@
+strings = input().split()
+n = int(input())
+
+def solution(strings, n):
+    
+    return 
+
+if __name__ == "__main__":
+    print(solution(strings,n))
+
+"""
+https://docs.python.org/ko/3/howto/sorting.html
+sorted와 sort에 대해서 제대로 알고 있어야 풀기 편할듯. 공부 ㄱㄱ
+"""
