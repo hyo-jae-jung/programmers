b86634fcba47f1dd8bdc70773ed56ada47b8035e
hyojaejung <hyojaejung@kakao.com>
2022-10-06 07:21:10 +0900

    소수 찾기:에라토스테네스의 체

--- 소수_찾기.py
+++ 소수_찾기.py
@@ -2,21 +2,32 @@ n = int(input())
 
 # from itertools import takewhile
 
-# def solution(n):
+# def solution1(n):
 #     return sum([True for i in range(2,n+1) if len([j for j in takewhile(lambda x:i%x!=0,range(2,i))])==i-2])
 
 
-def solution(n):
-    def sosu(n):
-        for i in range(2,n):
-            if n%i==0:return False
-        else:return True
-    sosu_list = []
-    for i in range(2,n+1):
-        sosu_list.append(sosu(i))
-    return(sum(sosu_list))
+# def solution2(n):
+#     def sosu(n):
+#         for i in range(2,n):
+#             if n%i==0:return False
+#         else:return True
+#     sosu_list = []
+#     for i in range(2,n+1):
+#         sosu_list.append(sosu(i))
+#     return(sum(sosu_list))
 
 # 에라토스테네스의 체로 풀어야 하는 문제
+
+def solution(n):
+    a = [False,False] + [True]*(n-1)
+    primes=[]
+
+    for i in range(2,n+1):
+        if a[i]:
+            primes.append(i)
+            for j in range(2*i, n+1, i):
+                a[j] = False
+    return len(primes)
 
 if __name__ == "__main__":
     print(solution(n))
