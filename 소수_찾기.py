f2dadccfe337876ee32a9f672d6ffdf1b4a08ec3
hyojaejung <hyojaejung@kakao.com>
2022-10-06 06:51:09 +0900

    소수는 에라토스테네스의 체를 알야아 할 듯

--- 소수_찾기.py
+++ 소수_찾기.py
@@ -1,7 +1,22 @@
 n = int(input())
 
+# from itertools import takewhile
+
+# def solution(n):
+#     return sum([True for i in range(2,n+1) if len([j for j in takewhile(lambda x:i%x!=0,range(2,i))])==i-2])
+
+
 def solution(n):
-    return [[i for i in range(2,j+1) if j%i] for j in range(2,n+1)]
+    def sosu(n):
+        for i in range(2,n):
+            if n%i==0:return False
+        else:return True
+    sosu_list = []
+    for i in range(2,n+1):
+        sosu_list.append(sosu(i))
+    return(sum(sosu_list))
+
+# 에라토스테네스의 체로 풀어야 하는 문제
 
 if __name__ == "__main__":
     print(solution(n))
