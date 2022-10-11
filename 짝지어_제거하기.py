7eb9ba4a4bd84d95c1ba0a727d5227adefc3b6be
hyojaejung <hyojaejung@kakao.com>
2022-10-12 00:25:12 +0900

    짝지어 제거하기(실패)

--- 짝지어_제거하기.py
+++ 짝지어_제거하기.py
@@ -1,25 +1,29 @@
 s = input()
 
-def solution(s):
+# def solution(s):
 
-    if len(s)%2==1:
-        return 0
+#     if len(s)%2==1:
+#         return 0
 
-    for i in list(set(s)):
-        if s.count(i)%2 == 1:
-            return 0
+#     for i in list(set(s)):
+#         if s.count(i)%2 == 1:
+#             return 0
 
-    s_list = list(s)
+#     origin = list(s)
+#     s_list = origin
+#     while origin:
+#         for i in reversed(range(len(s_list)-1)):
+#             if s_list[i] == s_list[i+1]:
+#                 s_list.pop(i)
+#                 s_list.pop(i)
+#         if origin == s_list:
+#             return 0
+#         else:
+#             origin = s_list
 
-    while s_list:
-        start = s_list
-        for i in range(len(s_list)-1):
-            if s_list[i] == s_list[i+1]:
-                s_list.pop(i)
-                s_list.pop(i)
-        if start == s_list:
-            return 0
-    return 1
+#     return 1
+
+# 실패
 
 if __name__ == "__main__":
     print(solution(s))
