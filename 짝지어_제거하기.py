5f90a2d8556e54e88c52b26cb5ff14b7bdc4e546
hyojaejung <hyojaejung@kakao.com>
2022-10-13 23:17:04 +0900

    짝지어 제거하기 실패2

--- 짝지어_제거하기.py
+++ 짝지어_제거하기.py
@@ -23,7 +23,34 @@ s = input()
 
 #     return 1
 
-# 실패
+# # 실패1
+
+# from collections import deque
+
+# def solution(s):
+#     deq = deque(s)
+#     if len(deq)%2==0 and 1 not in [deq.count(i)%2 for i in set(deq)]:
+#         cnt = 0
+#         while deq:
+#             print(deq)
+#             if deq[-1] == deq[-2]:
+#                 deq.pop()
+#                 deq.pop()
+#                 cnt = 0
+#             else:
+#                 deq.rotate(1)
+#                 cnt+=1
+#                 if cnt == len(deq):
+#                     return 0
+#         return 1
+#     else:
+#         return 0
+# 실패2
+
+def solution(s):
+    s_list = list(s)
+    if len(deq)%2==0 and 1 not in [deq.count(i)%2 for i in set(deq)]:
+    return
 
 if __name__ == "__main__":
     print(solution(s))
